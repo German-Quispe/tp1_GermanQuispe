90b75c8cee7590bc0becab7a67f664aacec43367
reneq <reneq@German19>
2024-04-10 11:01:58 -0300

    Se creo el repo del tp1, el gitignore y el ejercicio numero 1.

---
+++ tp1_GermanQuispe/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//declaraciony asignacion de las variables 
+		int n1 = 5;
+		int n2 = 10;
+		int n3 = 8;
+		int n4 = 6;
+		int n5 = 3;
+		
+		//Declaracion de la variable promedio y su respectivo valor
+		float promedio = (float) (n1+n2+n3+n4+n5)/5; //conversion explicita 
+		
+		//Muestra de consola
+		System.out.println("El promedio es: "+promedio);
+	}
+}
