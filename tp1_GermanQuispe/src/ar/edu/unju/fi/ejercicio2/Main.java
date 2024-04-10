9f0112f43520bdd1b7a516cbc7a9f6b998e2eb81
reneq <reneq@German19>
2024-04-10 11:12:35 -0300

    Ejercicio 2 realizado.

---
+++ tp1_GermanQuispe/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//declaracion y asignacion de variables
+		
+		String nombrePais = "Argentina";
+		int edadPersona = 23;
+		float alturaEdificio = 583.5f;
+		float precioProducto = 11659.78f;
+		long numeroTelefono = 3884106636l;
+		double coseno = Math.cos(0.5); 
+		
+		//Muestra de los datos de las variables
+		
+		System.out.println("EL nombre del pais es: " + nombrePais);
+		System.out.println("La edad de la persona es: " + edadPersona);
+		System.out.println("La altura de edificio es: " + alturaEdificio);
+		System.out.println("El precio del producto es: " + precioProducto);
+		System.out.println("El numero de telefono es: " + numeroTelefono);
+		System.out.println("El coseno de 0.5 es: " + coseno);
+	}
+
+}
