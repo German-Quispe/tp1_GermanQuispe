22a49336c1110693e452211d04b942912a74dc5c
reneq <reneq@German19>
2024-04-19 21:48:34 -0300

    Ejercicio 11 terminado. Implementacion del for().

---
+++ tp1_GermanQuispe/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Uso del metodo "for" para mostrar la secuencia numerica
+		for (int j = 40, i = 2; j > 0; j--, i++) {
+			System.out.print(j*i+" ");
+		}
+	}
+
+}
