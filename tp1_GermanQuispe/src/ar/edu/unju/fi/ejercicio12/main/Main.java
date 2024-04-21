19f2080a49bde188f55c7e240e2d58dbc2dc8378
reneq <reneq@German19>
2024-04-21 06:14:10 -0300

    Modificaciones en el ingreso

--- tp1_GermanQuispe/src/ar/edu/unju/fi/ejercicio12/main/Main.java
+++ tp1_GermanQuispe/src/ar/edu/unju/fi/ejercicio12/main/Main.java
@@ -1,5 +1,6 @@
 package ar.edu.unju.fi.ejercicio12.main;
 
+
 import java.util.Calendar;
 import java.util.Scanner;
 
@@ -10,21 +11,23 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
 		//------------Ingresos de los datos---------------
 		System.out.println("Ingrese nombre:");
 		String nombre = sc.nextLine();
-		System.out.println("Ingrese fecha de naciomiento:");
-		System.out.println("Año(yyyy): ");
-		int anio = sc.nextInt();
-		System.out.println("Mes(MM): ");
-		int mes = sc.nextInt();
+		System.out.println("Ingrese fecha de naciomiento: ");
 		System.out.println("Dia(dd): ");
 		int dia = sc.nextInt();
+		System.out.println("Mes(MM): ");
+		int mes = sc.nextInt();
+		System.out.println("Año(yyyy): ");
+		int anio = sc.nextInt();
 		//------------------------------------------------
-		//Instanciamos calendar 
+		//Instanciamos calendar
 		Calendar fechaNacimiento = Calendar.getInstance();
 		//ingresames una fecha especifica 
-		fechaNacimiento.set(anio, mes, dia);
+		fechaNacimiento.set(anio,mes,dia);
+		
 		
 		Persona persona = new Persona(nombre, fechaNacimiento);
 		
